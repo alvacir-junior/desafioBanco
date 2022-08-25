21e110ae345c9f0f6da7b77f72a863c778ef352c
alvacir-junior <alvacir.junior98@gmail.com>
2022-08-24 21:36:24 -0300

    desafio

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+		
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+
+		cc.depositar(200);
+		cc.transferir(150, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
